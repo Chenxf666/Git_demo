dc558420fb23752204d2351cacfd4f456f9ecddd
Chenli <chenli897666@163.com>
2023-01-07 18:30:24 +0800

    second commit

--- src/main/java/com/txr/git/GitTest.java
+++ src/main/java/com/txr/git/GitTest.java
@@ -6,6 +6,7 @@ package com.txr.git;
  */
 public class GitTest {
     public static void main(String[] args){
-        System.out.println("hello");
+        System.out.println("hello1");
+        System.out.println("hello2");
     }
 }
